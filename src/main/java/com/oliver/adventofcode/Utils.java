61b9cbdc4f11af6b78e46af28da93ac4b99fdb70
oliverknowles <oliverknowles1@gmail.com>
2017-12-07 17:29:01 +0000

    Moved day5 and day7 input into separate resource files. Added utils method to read file

--- src/main/java/com/oliver/adventofcode/Utils.java
+++ src/main/java/com/oliver/adventofcode/Utils.java
@@ -1,6 +1,41 @@
 package com.oliver.adventofcode;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Utils {
+
+    public static List<String> readFile(int day) {
+        File file = getFile("Day" + day + ".txt");
+        Scanner scanner = getScanner(file);
+        List<String> lines = new ArrayList<>();
+        while (scanner.hasNext()) {
+            lines.add(scanner.nextLine());
+        }
+        return lines;
+    }
+
+    private static Scanner getScanner(File file) {
+        try {
+            return new Scanner(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static File getFile(String fileName) {
+        try {
+            URI resourceURI = ClassLoader.getSystemClassLoader().getResource(fileName).toURI();
+            return new File(resourceURI);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static int[] stringToSingleDigitArray(String input) {
         int[] result = new int[input.length()];
