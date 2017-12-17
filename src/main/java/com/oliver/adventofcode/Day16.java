952bc83c2cbc99124e201f27728afee58be2e62a
Oliver Knowles <oliverknowles1@gmail.com>
2017-12-17 22:39:12 +0000

    Added solution for day 16

---
+++ src/main/java/com/oliver/adventofcode/Day16.java
@@ -0,0 +1,100 @@
+package com.oliver.adventofcode;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Day16 {
+
+    private Map<Character, Integer> programPosition;
+    private char[] initialOrdering;
+    private int cycleLength;
+
+    public Day16(int numPrograms) {
+        initialOrdering = new char[numPrograms];
+        programPosition = new HashMap<>();
+        for (int i = 0; i < numPrograms; i++) {
+            char curChar = (char) ('a' + i);
+            programPosition.put(curChar, i);
+            initialOrdering[i] = curChar;
+        }
+    }
+
+    public void executeInstructions(List<String> instructions, int numLoops) {
+        for (int loop = 0; loop < numLoops; loop++) {
+            executeInstructions(instructions);
+            if (loop > 0 && originalOrdering()) {
+                cycleLength = loop + 1;
+                break;
+            }
+        }
+
+        if (cycleLength > 0) {
+            for (int i = 0; i < numLoops % cycleLength; i++) {
+                executeInstructions(instructions);
+            }
+        }
+    }
+
+    private boolean originalOrdering() {
+        for (Map.Entry<Character, Integer> entry : programPosition.entrySet()) {
+            Integer pos = entry.getValue();
+            if (initialOrdering[pos] != entry.getKey()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void executeInstructions(List<String> input) {
+        for (String instruction : input) {
+            if (instruction.startsWith("s")) {
+                int spinSize = Integer.parseInt(instruction.substring(1));
+                rotateToFront(spinSize);
+            } else if (instruction.startsWith("x")) {
+                String[] parts = instruction.substring(1).split("/");
+                int position1 = Integer.parseInt(parts[0]);
+                int position2 = Integer.parseInt(parts[1]);
+                swapPrograms(position1, position2);
+            } else if (instruction.startsWith("p")) {
+                char program1 = instruction.charAt(1);
+                char program2 = instruction.charAt(3);
+                swapPrograms(program1, program2);
+            }
+        }
+    }
+
+    private void rotateToFront(int spinSize) {
+        int offset = programPosition.size() - spinSize;
+        for (Map.Entry<Character, Integer> entry : programPosition.entrySet()) {
+            Integer curPos = entry.getValue();
+            Integer newPos = (curPos < offset) ? curPos + spinSize : curPos - offset;
+            programPosition.put(entry.getKey(), newPos);
+        }
+    }
+
+    private void swapPrograms(int position1, int position2) {
+        for (Map.Entry<Character, Integer> entry : programPosition.entrySet()) {
+            if (entry.getValue() == position1) {
+                programPosition.put(entry.getKey(), position2);
+            } else if (entry.getValue() == position2) {
+                programPosition.put(entry.getKey(), position1);
+            }
+        }
+    }
+
+    private void swapPrograms(char program1, char program2) {
+        int position1 = programPosition.get(program1);
+        int position2 = programPosition.get(program2);
+        programPosition.put(program1, position2);
+        programPosition.put(program2, position1);
+    }
+
+    public String getProgramOrderString() {
+        char[] chars = new char[programPosition.size()];
+        for (Map.Entry<Character, Integer> entry : programPosition.entrySet()) {
+            chars[entry.getValue()] = entry.getKey();
+        }
+        return new String(chars);
+    }
+}
---
+++ src/test/groovy/com/oliver/adventofcode/Day16Test.groovy
@@ -0,0 +1,54 @@
+package com.oliver.adventofcode
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+
+class Day16Test extends Specification {
+
+    private static final List<String> INPUT = Arrays.asList(Utils.readFile(16)[0].split(","))
+
+    private Day16 underTest
+
+    def "run day 16 first half"() {
+        given:
+            underTest = new Day16(16)
+            underTest.executeInstructions(INPUT, 1)
+
+        when:
+            String firstHalf = underTest.getProgramOrderString()
+
+        then:
+            println "first half = $firstHalf"
+    }
+
+    def "run day 16 second half"() {
+        given:
+            underTest = new Day16(16)
+            underTest.executeInstructions(INPUT, 999_999_999)
+
+        when:
+            String secondHalf = underTest.getProgramOrderString()
+
+        then:
+            println "second half = $secondHalf"
+    }
+
+    @Unroll
+    def "successfully return final order after #numIterations iterations of instructions"() {
+        given:
+            underTest = new Day16( 5)
+            underTest.executeInstructions(["s1", "x3/4", "pe/b"], numIterations)
+
+        when:
+            String result = underTest.getProgramOrderString()
+
+        then:
+            result == expectedResult
+
+        where:
+            numIterations | expectedResult
+            1             | "baedc"
+            2             | "ceadb"
+    }
+}
