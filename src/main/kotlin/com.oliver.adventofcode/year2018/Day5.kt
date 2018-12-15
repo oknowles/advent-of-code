ddb692d82620cab11a57c2289f436d240b682233
Oliver Knowles <oliver.knowles@gmail.com>
2018-12-15 00:25:25 +0000

    Added (slow) solution for day 5

---
+++ src/main/kotlin/com.oliver.adventofcode/year2018/Day5.kt
@@ -0,0 +1,53 @@
+package com.oliver.adventofcode.year2018
+
+import com.oliver.adventofcode.readFile
+
+class Day5(private val polymer: String) {
+    companion object {
+        @JvmStatic
+        fun fromInput(): Day5 {
+            return Day5(readFile(5)[0])
+        }
+        private const val CHAR_DIFF = 'a' - 'A'
+    }
+
+    fun getUnitCountAfterFolding(): Int {
+        return collapsePolymer(polymer).length
+    }
+
+    fun collapsePolymer(polymer: String): String {
+        var result = polymer
+        var charIndex = 0
+        while (charIndex < result.length - 1) {
+            if (Math.abs(result[charIndex] - result[charIndex + 1]) == CHAR_DIFF) {
+                result = result.removeRange(charIndex, charIndex + 2)
+                if (charIndex > 0) {
+                    charIndex--
+                }
+            } else {
+                charIndex++
+            }
+        }
+        return result
+    }
+
+    fun findSmallestPolymer(): Int {
+        var smallestPolymer = Integer.MAX_VALUE
+        for (char in 'A' .. 'Z') {
+            var temp = polymer
+            var charIndex = 0
+            while (charIndex < temp.length) {
+                if (temp[charIndex] == char || temp[charIndex] == char + CHAR_DIFF) {
+                    temp = temp.removeRange(charIndex, charIndex + 1)
+                } else {
+                    charIndex++
+                }
+            }
+            val collapsedLength = collapsePolymer(temp).length
+            if (collapsedLength < smallestPolymer) {
+                smallestPolymer = collapsedLength
+            }
+        }
+        return smallestPolymer
+    }
+}
